37c805e38df4e4d0a8d7ce23ad7fba0def6aeaf6
Marc <foxtrot@malloc.me>
2022-01-20 17:27:41 +0000

    foxtrot: restore keychron k6 keymap

--- keyboards/keychron/k6/keymaps/ansi/keymap.c
+++ keyboards/keychron/k6/keymaps/ansi/keymap.c
@@ -29,32 +29,75 @@ enum layer_names {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
- /*      Row:         0          1          2          3        4         5        6         7        8        9          10         11         12         13         14         15        */
-      [_WINDOWS] = { {   KC_GESC,   KC_1,      KC_2,      KC_3,    KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_NO,     RGB_MOD   },
-                  {   KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLASH, KC_NO,     KC_HOME   },
-                  {   KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_PGUP   },
-                  {   KC_LSFT,   KC_NO,     KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_PGDOWN },
-                  {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RCTRL,  MO(_FN1),  MO(_FN2),  KC_LEFT,   KC_DOWN,   KC_RGHT   }
-                },
-      [_MAC] = { {   KC_GESC,   KC_1,      KC_2,      KC_3,    KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_NO,     RGB_MOD   },
-                  {   KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLASH, KC_NO,     KC_HOME   },
-                  {   KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_PGUP   },
-                  {   KC_LSFT,   KC_NO,     KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_PGDOWN },
-                  {   KC_LCTL,   KC_LALT,   KC_LGUI,   KC_NO,   KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RCTRL,  MO(_FN1),  MO(_FN2),  KC_LEFT,   KC_DOWN,   KC_RGHT   }
-                },
-      [_FN1] = {  {   KC_GESC,   KC_SLCK,   KC_PAUS,   KC_APP,  KC_NO,    RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_NO,     KC_NO,     KC_PGUP    },
-                  {   KC_NO,     RGB_M_P,   RGB_M_B,   RGB_M_R, RGB_M_SW, RGB_M_T, RGB_SPD,  RGB_SPI, KC_NO,   KC_NO,     KC_INS,    KC_DEL,    KC_END,    KC_NO,     KC_NO,     KC_PGDOWN  },
-                  {   KC_NO,     KC_VOLD,   KC_VOLU,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     RGB_SAD,   RGB_SAI,   KC_NO,     KC_NO,     KC_NO,     KC_PGUP    },
-                  {   KC_NO,     KC_NO,     KC_NO,     RGB_TOG, KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   RGB_RMOD,  RGB_MOD,   KC_NO,     KC_NO,     KC_NO,     RGB_VAI,   KC_PGDOWN  },
-                  {   KC_GRV,    KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   RESET,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     RGB_HUD,   RGB_VAD,   RGB_HUI    }
-               },
-      [_FN2] = {  {   KC_GESC,   KC_F1,     KC_F2,     KC_F3,   KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_NO,     KC_PGUP    },
-                  {   KC_NO,     RGB_M_P,   RGB_M_B,   RGB_M_R, RGB_M_SW, RGB_M_T, RGB_SPD,  RGB_SPI, KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_PGDOWN  },
-                  {   KC_NO,     KC_VOLD,   KC_VOLU,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     RGB_SAD,   RGB_SAI,   KC_NO,     KC_NO,     KC_NO,     KC_PGUP    },
-                  {   KC_NO,     KC_NO,     KC_NO,     RGB_TOG, KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   RGB_RMOD,  RGB_MOD,   KC_NO,     KC_NO,     KC_NO,     RGB_VAI,   KC_PGDOWN  },
-                  {   KC_GRV,    KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   RESET,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     RGB_HUD,   RGB_VAD,   RGB_HUI    }
-               }
+/*
+ *   0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+ * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ + │ BKSPC │DEL│
+ * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┼───┤
+ * │TAB │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \   │HME│
+ * ├────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴──────┼───┤
+ * │CAPS │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ” │ RETURN  │END│
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┬───┼───┤
+ * │SHFT  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │  SHIFT │ ^ │INS│
+ * ├────┬─┴──┬┴───┼───┴───┴───┴───┴───┴───┴┬──┴┬──┴┬───┬───┼───┼───┤
+ * │CTRL│ OS │ALT │                        │ALT│FN2│FN1│ < │ v │ > │
+ * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+*   Row:         0          1          2          3        4         5        6         7        8        9          10         11         12         13         14         15        */
+    [_WINDOWS] = {
+            {   KC_GESC,   KC_1,      KC_2,      KC_3,    KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_NO,     KC_DEL    },
+            {   KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLASH, KC_NO,     KC_HOME   },
+            {   KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_END    },
+            {   KC_LSFT,   KC_NO,     KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_INS    },
+            {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RCTRL,  MO(_FN2),  MO(_FN1),  KC_LEFT,   KC_DOWN,   KC_RGHT   }
+        },
+    [_MAC] = {
+            {   KC_GESC,   KC_1,      KC_2,      KC_3,    KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_NO,     KC_DEL    },
+            {   KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLASH, KC_NO,     KC_HOME   },
+            {   KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_END    },
+            {   KC_LSFT,   KC_NO,     KC_Z,      KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_INS    },
+            {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RCTRL,  MO(_FN2),  MO(_FN1),  KC_LEFT,   KC_DOWN,   KC_RGHT   }
+        },
+/*
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+ * │ESC│F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│       │TOG│ TOG = RGB Toggle
+ * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┼───┤
+ * │    │RMD│MOD│PLN│BRT│RNB│SWL│SNK│KNT│XMS│GRD│SPD│SPI│      │   │ RGB Back, RGB Fwd, Plain, Breathe, Rainbow, Swirl, Snake, Knight, Xmas, Gradient, Speed Down, Speed Up
+ * ├────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴──────┼───┤
+ * │     │VLD│VLU│   │   │   │   │SAD│SAI│VAD│VAI│   │         │PGU│ Volume Down, Volume Up     Saturation Down, Saturation Up, Brightness Down, Brightness Up    Page Up  
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┬───┼───┤
+ * │      │   │   │   │   │   │   │   │   │HUD│HUI│        │   │PGD│ Hue Down, Hue Up    Page Down
+ * ├────┬─┴──┬┴───┼───┴───┴───┴───┴───┴───┴┬──┴┬──┴┬───┬───┼───┼───┤
+ * │    │    │    │          RESET         │   │   │   │   │   │   │
+ * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+*   Row:         0          1          2          3        4         5        6         7        8        9          10         11         12         13         14         15        */
+    [_FN1] = {
+            {   KC_GRAVE,  KC_F1,     KC_F2,     KC_F3,   KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_NO,     RGB_TOG    },
+	        {   KC_NO,     RGB_RMOD,  RGB_MOD,   RGB_M_P, RGB_M_B,  RGB_M_R,  RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X,   RGB_M_G,   RGB_SPD,   RGB_SPI,   KC_NO,     KC_NO,     KC_NO      },
+            {   KC_NO,     KC_VOLD,   KC_VOLU,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    RGB_SAD,  RGB_SAI, RGB_VAD,   RGB_VAI,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_PGUP    },
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,     RGB_HUD,   RGB_HUI,   KC_NO,     KC_NO,     KC_NO,     KC_PGDOWN  },
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,    RESET,    KC_NO,    KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO      }
+        },
+/*
+ *   0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+ * │   │   │   │   │   │   │   │   │   │   │   │   │   │       │   │
+ * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┼───┤
+ * │    │   │   │   │   │   │   │   │   │   │   │   │   │      │   │
+ * ├────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴──────┼───┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │         │   │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┬───┼───┤
+ * │      │   │   │   │   │   │   │   │   │   │   │        │   │   │
+ * ├────┬─┴──┬┴───┼───┴───┴───┴───┴───┴───┴┬──┴┬──┴┬───┬───┼───┼───┤
+ * │    │    │    │                        │   │   │   │   │   │   │
+ * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+*   Row:         0          1          2          3        4         5        6         7        8        9          10         11         12         13         14         15        */
+    [_FN2] = {
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO      },
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO      },
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO      },
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO      },
+            {   KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO      }
+        }
 };
 
 void dip_switch_update_user(uint8_t index, bool active){
